12ff6bcb9b72656052b53733bbda3b51cbf806a1
Taslim Oseni <taslimoseni@Taslims-MacBook-Pro.local>
2021-01-14 01:59:51 +0100

    Added fragments with navigation component

--- app/src/main/java/com/fairtask/ui/activities/MainActivity.kt
+++ app/src/main/java/com/fairtask/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fairtask
+package com.fairtask.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fairtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
